323312b3857fd6c964c6f4cbab02c16dfca19291
ahmadtourei <ahmad9@vt.edu>
2024-11-21 12:19:30 -0700

    update readme.md

--- source/train_model/train_ae.sh
+++ source/train_model/train_ae.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-#SBATCH -N1
 #SBATCH --ntasks-per-node=1
-#SBATCH -t 144:00:00
+#SBATCH -t 48:00:00
 #SBATCH -A 2206240948
 #SBATCH --output=/u/pa/nb/tourei/scratch/sits/ae_anomaly_detection/outputs/train/dec22/first_week/output_MPI_%j.out
 #SBATCH --mem-per-cpu=16G
@@ -14,7 +13,7 @@ source /u/pa/nb/tourei/anaconda3/condabin/conda
 source activate mpih5py
 
 # Run the script
-python train.py
+python train_ae.py
 
 # Print end time
 echo "Job ended at: $(date)"
